36a0adf5d95117319a5473e087ccb10381577018
YaTut1901 <ovanya1901@gmail.com>
2024-02-11 22:07:54 +0200

    remove js files left after merge

--- src/assets/assets.js
+++
@@ -1,5 +0,0 @@
-import coinbase from './coinbase.svg';
-import metamask from './metamask.svg';
-import walletConnect from './walletConnect.svg';
-
-export { coinbase, metamask, walletConnect };
--- src/util/provider/ethereum/ETHPriceProvider.js
+++
@@ -1,8 +0,0 @@
-import ethPrice from "eth-price";
-
-async function getETHPrice() {
-    const price = await ethPrice("usd");
-    return Math.round(price[0].slice(5));
-}
-
-export default getETHPrice;
